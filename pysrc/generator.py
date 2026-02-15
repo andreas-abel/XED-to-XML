c49a19675193e11f6296cc6f8130bc6b2e259834
Andreas Abel <mail@andreasabel.de>
2026-02-15 16:29:30 +0100

    Add no_src_dest_match attribute

--- pysrc/generator.py
+++ pysrc/generator.py
@@ -716,6 +716,8 @@ def generateXMLFile(agi):
                                     XMLInstr.attrib['locked'] = '1'
                                  if 'MXCSR' in ii.attributes:
                                     XMLInstr.attrib['mxcsr'] = '1'
+                                 if 'NO_SRC_DEST_MATCH' in ii.attributes:
+                                    XMLInstr.attrib['no_src_dest_match'] = '1'
 
                               if ii.iform_enum.endswith('_ZU'):
                                  stringSuffix += '_ZU'
